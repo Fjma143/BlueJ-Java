49329be04ae9687a602488449963ff722d08c89c
Ricardo Gottheil <ricardogottheil@gmail.com>
2015-03-31 18:20:05 -0500

    Idea sin terminar

--- Taller7/Taller7.java
+++ Taller7/Taller7.java
@@ -7,7 +7,7 @@
 import java.util.Scanner;
 public class Taller7
 {
-    public void factorial(int n){    // Punto 1   --- SIN TERMINAR ---
+    public void factorial(int n){    // Punto 1   
         int f = 1;
         int i=0;
         while(n != 0){
@@ -21,5 +21,13 @@ public class Taller7
     public void potencia(int base, int n){
        int r = (int) Math.pow(base,n);
         System.out.println(base+" elevado a la "+n+" es "+r+".");
+    }
+    
+    public double sen(double x, int n){
+        double acum=0;
+        for (int i=0;i<n;i++){
+            acum += Math.pow(x,2*i+1)*(Math.pow(-1,1))/ factorial(2*i+1);
+        }
+        return acum;
     }
 }
